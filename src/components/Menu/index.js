d46740ac8c3fad50c98ac58363801b876dac3b74
Jheimisson Sampaio <jheimisson.sampaio@gmail.com>
2020-03-30 00:30:02 -0300

    Adicionando menu principal

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,15 +1,40 @@
 import React from 'react';
 
-// import QRCode from 'react-native-qrcode-image';
+import QRCode from 'react-native-qrcode-image';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import {Container, Code} from './styles';
+import {Container, Code, Nav, NavItem, NavText, SignOutButton, SignOutButtonText} from './styles';
 
 export default function Menu() {
   return (
     <Container>
       <Code>
-        {/* <QRCode value='https://google.com' size={80} bgColor='#FFFFFF' fgColor='#8b10ae'/> */}
+        <QRCode value='https://google.com' size={80} fgColor='#FFFFFF' bgColor='#8b10ae'/>
       </Code>
+      <Nav>
+        <NavItem>
+          <Icon name="help-outline" size={20} color="#FFF" />
+          <NavText>Me ajuda</NavText>
+        </NavItem>
+
+        <NavItem>
+          <Icon name="person-outline" size={20} color="#FFF" />
+          <NavText>Perfil</NavText>
+        </NavItem>
+
+        <NavItem>
+          <Icon name="credit-card" size={20} color="#FFF" />
+          <NavText>Configurar cartão</NavText>
+        </NavItem>
+
+        <NavItem>
+          <Icon name="smartphone" size={20} color="#FFF" />
+          <NavText>Configurações do app</NavText>
+        </NavItem>
+      </Nav>
+      <SignOutButton onPress={() => {}}>
+        <SignOutButtonText>SAIR DO APP</SignOutButtonText>
+      </SignOutButton>
     </Container>
   );
 }
