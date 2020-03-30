2384c158e81272d92971e1274757e6453c738bfa
Jheimisson Sampaio <jheimisson.sampaio@gmail.com>
2020-03-29 23:34:02 -0300

    Adicionando tabs e card

---
+++ src/components/Menu/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// import QRCode from 'react-native-qrcode-image';
+
+import {Container, Code} from './styles';
+
+export default function Menu() {
+  return (
+    <Container>
+      <Code>
+        {/* <QRCode value='https://google.com' size={80} bgColor='#FFFFFF' fgColor='#8b10ae'/> */}
+      </Code>
+    </Container>
+  );
+}
--- src/components/Tabs/index.js
+++ src/components/Tabs/index.js
@@ -9,9 +9,26 @@ export default function Tabs() {
     <Container>
       <TabsContainer>
         <TabItem>
-          <Icon name="person-add" size="24" color="#FFF" />
+          <Icon name="person-add" size={24} color="#FFF" />
           <TabText>Indicar amigos</TabText>
         </TabItem>
+        <TabItem>
+          <Icon name="chat-bubble-outline" size={24} color="#FFF" />
+          <TabText>Cobrar</TabText>
+        </TabItem>
+        <TabItem>
+          <Icon name="arrow-downward" size={24} color="#FFF" />
+          <TabText>Depositar</TabText>
+        </TabItem>
+        <TabItem>
+          <Icon name="arrow-upward" size={24} color="#FFF" />
+          <TabText>Transferir</TabText>
+        </TabItem>
+        <TabItem>
+          <Icon name="lock" size={24} color="#FFF" />
+          <TabText>Bloquear cartão</TabText>
+        </TabItem>
+
       </TabsContainer>
     </Container>
   );
